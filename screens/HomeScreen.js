edd0d2d0c5d1479783edaf7b8a8c9abc4f119e65
Mithila Narmada <mithilanarmadaliyanage@gmial.com>
2024-07-18 17:18:53 +0530

    Project Setup and configure Tailwind css

---
+++ screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class HomeScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text className='text-red-700 bg-white'>HomeScreen</Text>
+
+      </View>
+    )
+  }
+}
+
+export default HomeScreen
