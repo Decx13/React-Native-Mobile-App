6f138e8b24c80e6b04c596de9e09bf7a9596a710
Mithila Narmada <mithilanarmadaliyanage@gmial.com>
2024-07-27 21:49:17 +0530

    get data from api and make visible in scroll area

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import Finder from './screens/Finder';
+import ItemScreen from './screens/itemScreen';
 
 
 const Stack = createNativeStackNavigator();
@@ -13,11 +14,11 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-         <Stack.Navigator>
+        <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Finder" component={Finder} />
+        <Stack.Screen name="ItemScreen" component={ItemScreen}/>
       </Stack.Navigator>
-
     </NavigationContainer>
   );
 }
--- components/ItemCardArea.js
+++ components/ItemCardArea.js
@@ -1,10 +1,16 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import { FontAwesome } from '@expo/vector-icons';
 import React from 'react'
+import { useNavigation } from '@react-navigation/native';
 
 const ItemCardArea = ({imageSrc,title,location,data}) => {
+  const navigation = useNavigation();
+
   return (
-    <TouchableOpacity className="rounded-md border border-gray-300 space-y-2 px-3 py-2 shadow-md bg-white w-[182px]">
+    <TouchableOpacity 
+    onPress = {() => navigation.navigate("ItemScreen", { param: data})}
+    className="rounded-md border border-gray-300 space-y-2 px-3 py-2 shadow-md bg-white w-[182px]">
+      
         <Image
             source={{uri: imageSrc}}
             className="w-full h-40 rounded-md object-cover"
